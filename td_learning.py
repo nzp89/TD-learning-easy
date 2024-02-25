1b0e73d3b42d12fee2de31cc3c17ad141fd56123
nzp89 <92073106+nzp89@users.noreply.github.com>
2024-02-25 18:15:00 +0900

    Add comments

--- td_learning.py
+++ td_learning.py
@@ -1,32 +1,41 @@
+# TD learning program
+
 import numpy as np
 import matplotlib.pyplot as plt
+
+# This is the constant of TD learning.
 alpha = 0.5
 num_steps = 23
 num_episodes = 70
 
+# This is the constant of reward.
 stim_time = 6
 reward_time = 20
 
+# Weight, stim, and reward are different for each time. (at first, all 0.)
 weight = np.zeros(num_steps)
-
 stim = np.zeros(num_steps)
-stim[stim_time] = 1
-
 reward = np.zeros(num_steps)
+
+# stim and reward are 1.
+stim[stim_time] = 1
 reward[reward_time] = 1
 
+# TD_errors and state_value definition.
 TD_errors = np.zeros(num_steps)
 state_values = np.zeros(num_steps)
 
+# print primary state.
 print(f"stim: {stim}")
 print(f"reward: {reward}")
 print(f"weight: {weight}")
 
+# plot setting (colormap is changeable.)
 plt.figure(tight_layout=True)
 colormap = plt.get_cmap("cool")
 
 for i in range(num_episodes):
-    state_values = np.zeros(num_steps)
+    state_values = np.zeros(num_steps) # you need to reset state_values each episodes.
     for time in range(num_steps - 1):
         for tau in range(time):
             state_values[time] += weight[tau] * stim[time - tau]
@@ -38,7 +47,7 @@ for i in range(num_episodes):
     print(f"state_values: {state_values}")
     print(f"TD_errors: {TD_errors}")
     print(f"weight: {weight}")
-    if i % 1 == 0:
+    if i % 1 == 0: # each episodes plot the result. (changeable.)
         plt.subplot(211, xlabel="time step", ylabel="TD error")
         plt.plot(TD_errors, color=colormap(i / num_episodes))
         plt.subplot(212, xlabel="time step", ylabel="state value")
